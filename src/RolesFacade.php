a2ee23783dd224fac53d9d68bdce6fb4d4a403b4
NESTORACUNA <38430159+NESTORACUNA@users.noreply.github.com>
2022-10-11 16:35:01 -0300

    Update RolesFacade.php
    editado por nestor

--- src/RolesFacade.php
+++ src/RolesFacade.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Facade;
 
 class RolesFacade extends Facade
 {
-    /**
+    /** probnadi
      * Gets the facade accessor.
      *
      * @return string The facade accessor.
